205c76a0a0f0d4e6a02dc82df00d0b6ebb6a6996
Jonas Wagner <>
2019-09-11 10:08:56 +0200

    1.3

---
+++ helper.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+    var lulz = document.getElementsByClassName("notion-help-button")[0];
+    lulz.setAttribute("id", "id-test");
+  
+    document.getElementById('id-test').style.bottom = "75px";
+
+    };
